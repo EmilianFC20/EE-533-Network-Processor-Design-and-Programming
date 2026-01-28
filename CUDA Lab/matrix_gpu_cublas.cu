0c373570887b6e2cc0408c9d45985011812d75c0
EM <fdezemi@outlook.es>
2026-01-28 02:19:28 -0800

    Git cublas updated and python script created to compile the results.

--- CUDA Lab/matrix_gpu_cublas.cu
+++ CUDA Lab/matrix_gpu_cublas.cu
@@ -1,89 +1,94 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <cuda_runtime.h>
+#include <cublas_v2.h> // For cuBLAS functions
 
-#define TILE_WIDTH 16 
- 
-__global__ void matrixMultiplyTiled(float *A, float *B, float *C, int N) { 
-    __shared__ float ds_A[TILE_WIDTH][TILE_WIDTH]; 
-    __shared__ float ds_B[TILE_WIDTH][TILE_WIDTH]; 
- 
-    int bx = blockIdx.x; int by = blockIdx.y; 
-    int tx = threadIdx.x; int ty = threadIdx.y; 
- 
-    int Row = by * TILE_WIDTH + ty; 
-    int Col = bx * TILE_WIDTH + tx; 
- 
-    float Pvalue = 0.0; 
-    for (int m = 0; m < (N + TILE_WIDTH - 1) / TILE_WIDTH; ++m) { 
-        if (Row < N && (m*TILE_WIDTH+tx) < N) 
-            ds_A[ty][tx] = A[Row * N + m * TILE_WIDTH + tx]; 
-        else 
-            ds_A[ty][tx] = 0.0f; 
- 
-        if (Col < N && (m*TILE_WIDTH+ty) < N) 
-            ds_B[ty][tx] = B[(m*TILE_WIDTH + ty) * N + Col]; 
-        else 
-            ds_B[ty][tx] = 0.0f; 
- 
-        __syncthreads(); 
- 
-        for (int k = 0; k < TILE_WIDTH; ++k) 
-            Pvalue += ds_A[ty][k] * ds_B[k][tx]; 
-        __syncthreads(); 
-    } 
- 
-    if (Row < N && Col < N) 
-        C[Row * N + Col] = Pvalue; 
+// Helper function for CUDA error checking
+void checkCudaError(cudaError_t err, const char *msg) {
+    if (err != cudaSuccess) {
+        fprintf(stderr, "CUDA Error: %s: %s\n", msg, cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
 }
+
+// Helper function for cuBLAS error checking
+void checkCublasError(cublasStatus_t status, const char *msg) {
+    if (status != CUBLAS_STATUS_SUCCESS) {
+        fprintf(stderr, "cuBLAS Error: %s\n", msg);
+        exit(EXIT_FAILURE);
+    }
+}
+
+// No custom kernel needed when using cuBLAS
 
 int main(int argc, char **argv) {
     int N = (argc > 1) ? atoi(argv[1]) : 1024; // allow matrix size as input
     size_t size = N * N * sizeof(float);
 
+    // Allocate host memory
     float *A = (float *)malloc(size);
     float *B = (float *)malloc(size);
     float *C = (float *)malloc(size);
 
     for (int i = 0; i < N * N; i++) {
-        A[i] = rand() % 100 / 100.0f;
-        B[i] = rand() % 100 / 100.0f;
+        A[i] = rand() / (float)RAND_MAX;
+        B[i] = rand() / (float)RAND_MAX;
     }
 
+    // Allocate device memory
     float *d_A, *d_B, *d_C;
-    cudaMalloc((void **)&d_A, size);
-    cudaMalloc((void **)&d_B, size);
-    cudaMalloc((void **)&d_C, size);
+    checkCudaError(cudaMalloc((void **)&d_A, size), "Malloc d_A");
+    checkCudaError(cudaMalloc((void **)&d_B, size), "Malloc d_B");
+    checkCudaError(cudaMalloc((void **)&d_C, size), "Malloc d_C");
 
-    cudaMemcpy(d_A, A, size, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_B, B, size, cudaMemcpyHostToDevice);
+    // Copy data from host to device
+    checkCudaError(cudaMemcpy(d_A, A, size, cudaMemcpyHostToDevice), "Memcpy A");
+    checkCudaError(cudaMemcpy(d_B, B, size, cudaMemcpyHostToDevice), "Memcpy B");
 
-    dim3 threadsPerBlock(16, 16);
-    dim3 blocksPerGrid(
-        (N + threadsPerBlock.x - 1) / threadsPerBlock.x,
-        (N + threadsPerBlock.y - 1) / threadsPerBlock.y
-    );
+    // --- cuBLAS Initialization ---
+    cublasHandle_t handle;
+    checkCublasError(cublasCreate(&handle), "cublasCreate");
+
+    // --- GEMM Parameters ---
+    // C = alpha * op(A) * op(B) + beta * C
+    const float alpha = 1.0f;
+    const float beta = 0.0f;
 
     cudaEvent_t start, stop;
-    cudaEventCreate(&start);
-    cudaEventCreate(&stop);
+    checkCudaError(cudaEventCreate(&start), "Create start event");
+    checkCudaError(cudaEventCreate(&stop), "Create stop event");
 
-    cudaEventRecord(start);
-    matrixMultiplyTiled<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, N);
-    cudaEventRecord(stop);
-
-    cudaEventSynchronize(stop);
+    checkCudaError(cudaEventRecord(start), "Record start");
+    
+    // --- cuBLAS sgemm call ---
+    // This performs the matrix multiplication: C = A * B
+    // Note on matrix ordering: cuBLAS assumes column-major format (like Fortran).
+    // Since our matrices are row-major (standard in C), computing C=A*B in row-major
+    // is equivalent to C_transpose = B_transpose * A_transpose in column-major.
+    // The simplest way to achieve this with cuBLAS is to switch the order of A and B
+    // in the function call.
+    checkCublasError(cublasSgemm(handle, 
+                                 CUBLAS_OP_N, CUBLAS_OP_N, 
+                                 N, N, N, 
+                                 &alpha, 
+                                 d_B, N, // B is first
+                                 d_A, N, // A is second
+                                 &beta, 
+                                 d_C, N), "cublasSgemm");
+    
+    checkCudaError(cudaEventRecord(stop), "Record stop");
+    checkCudaError(cudaEventSynchronize(stop), "Synchronize stop");
 
     float milliseconds = 0;
-    cudaEventElapsedTime(&milliseconds, start, stop);
+    checkCudaError(cudaEventElapsedTime(&milliseconds, start, stop), "Elapsed time");
 
-    cudaMemcpy(C, d_C, size, cudaMemcpyDeviceToHost);
+    // Copy result from device to host
+    checkCudaError(cudaMemcpy(C, d_C, size, cudaMemcpyDeviceToHost), "Memcpy C");
 
-    printf("GPU execution time (N=%d): %f seconds\n",
-           N, milliseconds / 1000.0f);
+    printf("cuBLAS execution time (N=%d): %f seconds\n", N, milliseconds / 1000.0f);
 
-    /* Cleanup */
+    // --- Cleanup ---
+    checkCublasError(cublasDestroy(handle), "cublasDestroy");
     cudaEventDestroy(start);
     cudaEventDestroy(stop);
     cudaFree(d_A);
